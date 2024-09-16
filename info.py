536ada14cd664fb2978f25857f3030163e0a3bd3
Yuvraj8698 <yuvrajsharwa355@gmail.com>
2024-09-16 20:10:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -111,14 +111,14 @@ if 'DYNO' in environ:
 else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0/0'))
-FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
+FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com' 'koyebapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
     "http://{}:{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'MUZAN_KILLER_Bot'))
+SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyPrincess'))
 MULTI_CLIENT = False
-name = str(environ.get('name', 'MUZAN_KILLER'))
+name = str(environ.get('name', 'LazyPrincess'))
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
 if 'DYNO' in environ:
     ON_HEROKU = True
